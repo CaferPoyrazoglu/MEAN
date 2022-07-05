4f904725b11e0a3f86b6e3b8d6a000b5d74b023e
CaferPoyrazoglu <cafererhan@gmail.com>
2022-07-05 17:16:38 +0300

    Fixes

--- frontend/src/app/layout/top-nav/top-nav.component.ts
+++ frontend/src/app/layout/top-nav/top-nav.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import {
+  Event,
+  NavigationCancel,
+  NavigationEnd,
+  NavigationError,
+  NavigationStart,
+  Router
+} from '@angular/router';
 import { TokenStorageService } from 'src/app/services/token-storage.service';
 
 @Component({
@@ -18,25 +25,40 @@ export class TopNavComponent {
   constructor(private tokenStorageService: TokenStorageService, private router: Router,) { }
 
   ngOnInit(): void {
-    this.isLoggedIn = !!this.tokenStorageService.getToken();
 
-    if (this.isLoggedIn) {
-      const user = this.tokenStorageService.getUser();
-      this.username = user.username;
-    }
 
-    if (this.isLoggedIn) {
-      const user = this.tokenStorageService.getUser();
-      this.isAdmin = user.isAdmin;
-    }
+    this.router.events.subscribe((event: Event) => {
+      switch (true) {
+        case event instanceof NavigationStart: {
 
+          break;
+        }
+
+        case event instanceof NavigationEnd:
+        case event instanceof NavigationCancel:
+        case event instanceof NavigationError: {
+
+          console.log(this.isLoggedIn);
+          this.isLoggedIn = !!this.tokenStorageService.getToken();
+          if (this.isLoggedIn) {
+            const user = this.tokenStorageService.getUser();
+            this.username = user.username;
+          }
+
+          if (this.isLoggedIn) {
+            const user = this.tokenStorageService.getUser();
+            this.isAdmin = user.isAdmin;
+          }
+          break;
+        }
+        default: {
+          break;
+        }
+      }
+    });
   }
-
   logout(): void {
     this.tokenStorageService.signOut();
-    this.router.navigate(['/login'])
-      .then(() => {
-        window.location.reload();
-      });
+    this.router.navigate(['/login']);
   }
 }
